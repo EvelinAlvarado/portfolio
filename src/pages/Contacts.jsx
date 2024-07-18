9b7d3dad087cdc2c0ab4ecfcf199dc8882dac25b
Evelin Alvarado <129849677+EvelinAlvarado@users.noreply.github.com>
2024-07-18 12:55:32 +0100

    Added styles tailwind CSS responsive design to Contacts Component

--- src/pages/Contacts.jsx
+++ src/pages/Contacts.jsx
@@ -9,21 +9,21 @@ export const Contacts = () => {
     <>
       {modalIsOpen ? <EmailModal setModalIsOpen={setModalIsOpen} /> : <></>}
       <div className="mx-[10%]">
-        <h1 className="text-[16vw] font-CormorantGaramond font-medium tracking-tight mb-3 titleAnimate">
+        <h1 className="text-[16vw] font-CormorantGaramond font-medium tracking-tight mb-3 titleAnimate 2xl:text-[250px]">
           {translations.contact.title}
         </h1>
         <div className="relative top-1 overflow-auto max-h-[calc(100vh-13rem)]">
-          <p className="text-xs w-full leading-6 tracking-[1px]">
+          <p className="text-[12px] w-full leading-6 tracking-[1px] sm:text-[13px]">
             {translations.contact.description}
           </p>
 
           <button
             onClick={() => setModalIsOpen(true)}
-            className="text-base tracking-[1px] font-CormorantGaramond bg-dark text-beige dark:bg-beige dark:text-dark py-2 px-4 rounded-full hover:px-[30px] transition-all duration-300 my-7 shadow-md"
+            className="text-[16px] tracking-[1px] font-CormorantGaramond bg-dark text-beige dark:bg-beige dark:text-dark py-2 px-4 rounded-full hover:px-[30px] transition-all duration-300 my-7 shadow-md sm:text-[18px]"
           >
             {translations.contact.button}
           </button>
-          <p className="text-xs w-full leading-6 tracking-[1px]">
+          <p className="text-[12px] w-full leading-6 tracking-[1px] sm:text-[13px]">
             {translations.contact.text}
             <a
               className="underline hover:none hover:line-through"
