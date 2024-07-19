8197bd22aaace557eaf51ffa5c575f1cd67d458a
Evelin Alvarado <129849677+EvelinAlvarado@users.noreply.github.com>
2024-07-19 16:34:56 +0100

    Update Vite config and LanguagesProvider for dynamic base URL

--- src/context/LanguagesContext.jsx
+++ src/context/LanguagesContext.jsx
@@ -11,7 +11,13 @@ export const LanguagesProvider = ({ children }) => {
   useEffect(() => {
     const fetchTranslations = async () => {
       try {
-        const response = await axios.get(`src/languages/${language}.json`);
+        // We use `import.meta.env.BASE_URL` to get the project's base URL.
+        // This is useful because the base URL can be different depending on whether you are working locally (development) or have deployed your application (production).
+        // Then, we concatenate this base URL with the path to the JSON file that contains the language translations.
+        const response = await axios.get(
+          `${import.meta.env.BASE_URL}src/languages/${language}.json`
+        );
+        console.log(import.meta.env.BASE_URL);
         console.log(response.data);
         setTranslations(response.data);
       } catch (error) {
--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,8 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react-swc";
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: "/", // Set to '/' if deploying at root, or '/subdirectory/' if deploying to a subdirectory
   plugins: [react()],
-})
+});
